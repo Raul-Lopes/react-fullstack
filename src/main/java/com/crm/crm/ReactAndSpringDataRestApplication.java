c662327cb9d8cb0b75aafbdbbe09a6d297098011
Raul Lopes <raullopes@sidero.ie>
2023-04-14 16:40:18 +0100

    first commit

---
+++ src/main/java/com/crm/crm/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,17 @@
+package com.crm.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+}
+
+
+/***
+ * http://localhost:8080/api/contacts
+ *
+ * **/
